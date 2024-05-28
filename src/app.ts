d6ce253557408f44988af52b8d7bed3c2c105d18
Arsen Abrahamyan <arsjj7@gmail.com>
2024-05-28 23:25:15 +0400

    set security improvment, implement helmet, limiter, express-mongo-sanitize, hpp

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,64 @@
 import express, { NextFunction, Request, Response } from "express";
+import hpp from "hpp";
+import helmet from "helmet";
 import morgan from "morgan";
+import AppError from "./utils/appError";
+import rateLimit from "express-rate-limit";
 import tourRouter from "./routes/tourRoutes";
 import userRouter from "./routes/userRoutes";
-import AppError from "./utils/appError";
-import { globalErrorHandler } from "./controllers/errorController";
+import ExpressMongoSanitize from "express-mongo-sanitize";
 import { protect } from "./controllers/authController";
+import { globalErrorHandler } from "./controllers/errorController";
 
 const app = express();
 
-//MIDDLEWARES
-app.use(morgan("dev"));
-app.use(express.json());
+//Set security HTTP headers
+app.use(helmet());
 
-app.use((req: Request, res: Response, next: NextFunction): void => {
-  next();
+// Development logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour",
 });
+app.use("/", limiter);
 
+//Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+//Data sanitization against NoSQL query injection
+app.use(ExpressMongoSanitize());
+
+//Data sanitization against XSS
+// app.use()
+
+//Prevent parameter polution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAvrage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
 
 //ROUTES
-app.use('/tours', protect, tourRouter)
-app.use('/users', userRouter)
+app.use("/tours", protect, tourRouter);
+app.use("/users", userRouter);
 
-app.all('*', (req: Request, res: Response, next: NextFunction) => {
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
   next(new AppError(`Can't find ${req.originalUrl} route in this server`, 404));
-})
+});
 
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
-
-
-export default app
-
-
+export default app;
